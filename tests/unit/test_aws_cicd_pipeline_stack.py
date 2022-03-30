a204ea673a767cd7b7558660f84d40a75dfe00e6
Tawanda Lloyd <tawanda@sohotech.io>
2022-03-30 08:39:14 +0200

    Initial commit

---
+++ tests/unit/test_aws_cicd_pipeline_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from aws_cicd_pipeline.aws_cicd_pipeline_stack import AwsCicdPipelineStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in aws_cicd_pipeline/aws_cicd_pipeline_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = AwsCicdPipelineStack(app, "aws-cicd-pipeline")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
