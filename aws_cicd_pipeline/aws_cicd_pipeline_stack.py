090f1ebf2f3cadae7e243eda38e9d050a7b79093
Mnqobi-Z <Mnqobi@solvyng.io>
2022-04-11 12:55:31 +0200

    added update

--- aws_cicd_pipeline/aws_cicd_pipeline_stack.py
+++ aws_cicd_pipeline/aws_cicd_pipeline_stack.py
@@ -39,7 +39,7 @@ class AwsCicdPipelineStack(cdk.Stack):
 
         pipeline_sg = get_pipeline_sg(
             self,
-            name = "ALB-SecurityGroup"
+            name = "pipeline_sg"
         )
 
         pipeline_tg = get_pipeline_tg(
