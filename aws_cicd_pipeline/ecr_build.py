9bf755d111cdce8dd7b40aefe0f9111df9e283f6
Mnqobi-Z <Mnqobi@solvyng.io>
2022-04-12 11:17:35 +0200

    updated

--- aws_cicd_pipeline/ecr_build.py
+++ aws_cicd_pipeline/ecr_build.py
@@ -1,7 +1,5 @@
 import aws_cdk.aws_ecr as ecr
 
-
-# repository_policy_text: Any
 def get_ecr_repo(self, name):
     return ecr.CfnRepository(self, "CfnRepository",
                              repository_name=name,
