8c3bd95da82f2505e39f33ad16d65da094902a2e
Mnqobi-Z <Mnqobi@solvyng.io>
2022-04-12 10:15:24 +0200

    updated

--- aws_cicd_pipeline/security_group.py
+++ aws_cicd_pipeline/security_group.py
@@ -1,7 +1,7 @@
 import aws_cdk.aws_ec2 as ec2
 
 
-def get_pipeline_sg(self, name):
+def get_pipeline_sg(self, name) -> ec2.SecurityGroup :
     return ec2.CfnSecurityGroupIngress(self, "CfnSecurityGroup",
                                        ip_protocol="tcp",
                                        cidr_ip="0.0.0.0/0",
