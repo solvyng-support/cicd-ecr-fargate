37148e91cad21beb0bf91b74328cd4f9383df274
Mnqobi-Z <Mnqobi@solvyng.io>
2022-04-13 14:53:31 +0200

    updated

--- aws_cicd_pipeline/cluster_fargate.py
+++ aws_cicd_pipeline/cluster_fargate.py
@@ -6,6 +6,7 @@ import aws_cdk.aws_ecs as ecs
 import aws_cdk.aws_ecr as ecr
 import aws_cdk.aws_elasticloadbalancingv2 as elbv2
 from aws_cicd_pipeline.load_balancer import get_app_lb, get_pipeline_tg
+from aws_cicd_pipeline.ecr_build import get_ecr_repo
 
 
 def cluster_fargate(self):
@@ -20,13 +21,13 @@ def cluster_fargate(self):
    # return ecs.FargateService(self, "Service", cluster=cluster_f, task_definition=task_d, lb=alb, lb_listener=alb_listener)
 
 
-def task_defination(self, ecr: ecr.Repository):    
+def task_defination(self, get_ecr_repo:ecr.CfnRepository):    
     return ecs.CfnTaskDefinition(self, "CfnTaskDefinition",
         container_definitions=[ecs.CfnTaskDefinition.ContainerDefinitionProperty( 
             depends_on=[ecs.CfnTaskDefinition.ContainerDependencyProperty(
                 container_name="catpipeline"  
             )],
-            image=ecr.repository_uri,
+            image=get_ecr_repo.attr_repository_uri,
             port_mappings=[ecs.CfnTaskDefinition.PortMappingProperty(
                 container_port=80,
                 protocol="tcp"
